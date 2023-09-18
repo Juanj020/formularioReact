402a13186fc4a63cc97a5df0aa40a485a2dc343a
juanj020 <juanj202122@gmail.com>
2023-09-18 13:32:13 -0500

    Listo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+let nextId = 0;
+
+export default function App() {
+  const [name, setName] = useState('');
+  const [artists, setArtists] = useState([]);
+  const [editingArtist, setEditingArtist] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const editArtist = (artist) => {
+    setEditingArtist(artist);
+    setName(artist.name);
+    setIsEditing(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className='encabezado'>
+      <h1>Escriba una palabra:</h1>
+      <input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          if (isEditing) {
+            const updatedArtists = artists.map((artist) =>
+              artist.id === editingArtist.id
+                ? { ...artist, name }
+                : artist
+            );
+            setArtists(updatedArtists);
+            setIsEditing(false);
+          } else {
+            setArtists([
+              ...artists,
+              { id: nextId++, name: name }
+            ]);
+          }
+          setName('');
+          setEditingArtist(null);
+        }}
+      >
+        {isEditing ? 'Guardar' : 'Añadir'}
+      </button>
+      <ul>
+        {artists.map((artist) => (
+          <li key={artist.id}>
+            {artist.name}{' '}
+            <button onClick={() => editArtist(artist)}>
+              Editar
+            </button>
+            <button
+              onClick={() => {
+                setArtists(
+                  artists.filter((a) => a.id !== artist.id)
+                );
+              }}
+            >
+              Eliminar
+            </button>
+          </li>
+        ))}
+      </ul>
+    </header>
   );
 }
-
-export default App;
